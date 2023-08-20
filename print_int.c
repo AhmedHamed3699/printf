e5ff77e48c2f514b2f394cc78c8f5542ff919c5b
mohamed_abdo <mohamed_abdo.com>
2023-08-20 15:10:29 -0700

    as

--- print_int.c
+++ print_int.c
@@ -10,12 +10,7 @@
  */
 int print_int(va_list args, flags_t *fg, modifiers_t *md)
 {
-	if (str == NULL)
-		return (-1)
-	char *str = va_arg(args, char *);
-	int i = 0;
+	int num = va_arg(args, int);
 
-	while (str[i] != '\0')
-		i++;
-	_putchar('0' + i);
+	return (_putchar('0' + num));
 }
