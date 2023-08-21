439193e72f9299194ffd5a41eeb1adff455c3b7d
mohamed_abdo <mohamed_abdo.com>
2023-08-21 07:44:40 -0700

    Edit_unsigned

--- print_unsigned.c
+++ print_unsigned.c
@@ -10,8 +10,15 @@
  */
 int print_unsigned(va_list args, flags_t *fg, modifiers_t *md)
 {
-	(void) args;
+	unsigned int num = va_arg(args, unsigned int), cont = 0;
+	char *array;
 	(void) fg;
 	(void) md;
-	return (0);
+
+	if (num == 0)
+		return (_putchar('0'));
+	array = convert_to_string(num, 10, 0);
+	cont += _puts(array);
+	free(array);
+	return (cont);
 }
