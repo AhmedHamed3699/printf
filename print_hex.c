6b712e7ab6272e3fee88e5594d59e79e5957491f
mohamed_abdo <mohamed_abdo.com>
2023-08-21 08:08:58 -0700

    Edit_hex

--- print_hex.c
+++ print_hex.c
@@ -11,10 +11,16 @@
  */
 int print_hex_small(va_list args, flags_t *fg, modifiers_t *md)
 {
-	(void) args;
+	unsigned int num = va_arg(args, unsigned int), cont = 0;
+	char *array;
 	(void) fg;
 	(void) md;
-	return (0);
+	if (num == 0)
+		return (_putchar('0'));
+	array = convert_to_string(num, 16, 1);
+	cont += _puts(array);
+	free(array);
+	return (cont);
 }
 
 /**
@@ -28,8 +34,14 @@ int print_hex_small(va_list args, flags_t *fg, modifiers_t *md)
  */
 int print_hex_capital(va_list args, flags_t *fg, modifiers_t *md)
 {
-	(void) args;
+	unsigned int num = va_arg(args, unsigned int), cont = 0;
+	char *array;
 	(void) fg;
 	(void) md;
-	return (0);
+	if (num == 0)
+		return (_putchar('0'));
+	array = convert_to_string(num, 16, 0);
+	cont += _puts(array);
+	free(array);
+	return (cont);
 }
