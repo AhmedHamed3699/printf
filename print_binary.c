1c35665b94c31c3c48071b724e38cf48478e00e4
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-08-21 17:15:01 +0300

    Betty related stuff

--- print_binary.c
+++ print_binary.c
@@ -10,21 +10,8 @@
  */
 int print_binary(va_list args, flags_t *fg, modifiers_t *md)
 {
-<<<<<<< HEAD
-	int num = va_arg(args, char*);
-	
-	while (num == 0)
-	{
-	
-		if (num % 2 == 0)
-		{
-			_putchar(0)
-		}
-	}
-=======
 	(void) args;
 	(void) fg;
 	(void) md;
 	return (0);
->>>>>>> 5fd16204c93822535864fc727227f2ed888c04cd
 }
